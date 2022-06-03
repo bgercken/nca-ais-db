9367fee5e3add64cfb0f7fe0b296901f4e09e48f
Bill Gercken <bgercken@comcast.net>
2022-06-03 15:56:23 -0400

    Add more test cases to sql.

--- console.sql
+++ console.sql
@@ -74,3 +74,25 @@ select sentence_id, field5 from rawFields where field5 not in ('A', 'B');
 
 select count(*) from rawData;
 
+-- message type
+select distinct field1 from rawFields;
+-- repeat indicator
+select distinct field2 from rawFields;
+-- fragment number
+select distinct field3 from rawFields;
+-- sequential message id
+select distinct field4 from rawFields;
+-- radio channel
+select distinct field5 from rawFields;
+
+
+-- looking for a correlation between radio channel C and the data
+select f.sentence_id, field1, field5, s_data from rawFields f, rawData d
+where f.sentence_id = d.sentence_id
+and field5 not in ('A', 'B');
+
+select * from classAPositionReport;
+-- check msg_type (msgID)
+select distinct msg_type from classAPositionReport;
+
+select distinct s_type, count(*) from rawData group by s_type;
